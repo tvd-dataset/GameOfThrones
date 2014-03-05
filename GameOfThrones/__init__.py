520c1a09056d551138c3d2fcb137f4e79f31a012
Hervé BREDIN <bredin@limsi.fr>
2014-03-05 19:30:55 +0200

    wip: matching new TVD plugin template

--- GameOfThrones/__init__.py
+++ GameOfThrones/__init__.py
@@ -34,27 +34,24 @@ del get_versions
 from tvd.series.plugin import SeriesPlugin
 import re
 import urllib3
-from tvd.common.graph import T
-import networkx as nx
+from tvd.common.graph import T, TStart, TEnd, AnnotationGraph
 
 
 class GameOfThrones(SeriesPlugin):
 
-    def outline(self, url, source=None, episode=None):
+    def outline(self, url=None, episode=None, **kwargs):
         """
         Parameters
         ----------
-        url : str
+        url : str, optional
             URL where resource is available
-        source : str, optional
-            Textual description of the source of the resource
         episode : Episode, optional
             Episode for which resource should be downloaded
             Useful in case a same URL contains resources for multiple episodes.
 
         Returns
         -------
-        g : AnnotationGraph
+        G : AnnotationGraph
         """
 
         http = urllib3.PoolManager()
@@ -64,16 +61,14 @@ class GameOfThrones(SeriesPlugin):
         nb_scene = 0
         word = r.data.split()
 
-        g = nx.MultiDiGraph(uri=str(episode), source=source)
-
+        G = AnnotationGraph()
+        t2 = TStart(episode=episode)
         sp = ""
 
         for w in word:
             if start_scene == 1:
                 sp = sp + " " + str(w)
             if (re.match("<h2><span", w) or re.match("NewPP", w)) and (nb_scene > 0):
-                t1 = T()
-                t2 = T()
                 usp = sp.decode("utf-8")
                 sp = usp.encode("ascii", "ignore")
                 sp = re.sub(re.compile('<i>', re.I), "", sp)
@@ -86,7 +81,16 @@ class GameOfThrones(SeriesPlugin):
                 sp = re.sub(re.compile('<p>',re.I), "", sp)
                 sp = re.sub(re.compile('</p>',re.I), "", sp)
                 sp = re.sub(re.compile('<h2><span',re.I), "", sp)
-                g.add_edge(t1, t2, scene="Scene_"+str(nb_scene), speech=sp)
+
+                # add /empty/ edge between previous and next annotations
+                t1 = T(episode=episode)
+                G.add_edge(t2, t1)
+
+                # add next annotation
+                t1 = t2
+                t2 = T(episode=episode)
+                G.add_edge(t1, t2, scene="Scene_"+str(nb_scene), speech=sp)
+
                 sp = ""
                 start_scene = 0
             if nb_scene > 0 and start_scene == 0 and re.match("<p>", w):
@@ -95,18 +99,23 @@ class GameOfThrones(SeriesPlugin):
             if re.match("id=\"Scene_(.*)\">Scene", w):
                 nb_scene = nb_scene + 1
 
-        return g
+        # add /empty/ edge between previous annotation and episode end
+        t1 = t2
+        t2 = TEnd(episode=episode)
+        G.add_edge(t1, t2)
 
-    def manual_transcript(self, url, source=None, episode=None):
+        return G
+
+    def manual_transcript(self, url=None, episode=None, **kwargs):
 
         http = urllib3.PoolManager()
         r = http.request('GET', url)
 
         start_scene = 0
-        nb_scene = 0
         word = r.data.split()
 
-        g = nx.MultiDiGraph(uri=str(episode), source=source)
+        G = AnnotationGraph()
+        t2 = TStart(episode=episode)
 
         sp = ""
 
@@ -136,8 +145,6 @@ class GameOfThrones(SeriesPlugin):
         word = re.split('####', sp)
         ok = 0
         for w in word:
-            t1 = T()
-            t2 = T()
             if ok == 1:
                 if re.match("(.*)</div>(.*)", w):
                     w = re.sub(re.compile('</div>(.*)</table>', re.UNICODE), "", w)
@@ -156,7 +163,18 @@ class GameOfThrones(SeriesPlugin):
                 ligne0 = uligne0.encode("ascii", "ignore")
                 uligne1 = ligne[1].decode("utf-8")
                 ligne1 = uligne1.encode("ascii", "ignore")
-                g.add_edge(t1, t2, spk=ligne0, speech=ligne1)
+
+
+                # add /empty/ edge between previous and next annotations
+                t1 = T(episode=episode)
+                G.add_edge(t2, t1)
+
+                # add next annotation
+                t1 = t2
+                t2 = T(episode=episode)
+                G.add_edge(t1, t2, spk=ligne0, speech=ligne1)
+
+
             if ok == 0 and (re.match("[a-zA-Z]{1,15}(\s[a-zA-Z]{1,15}){1,2} : (.*)", w) or re.match("[A-Z]{1,15}(\s[A-Z]{1,15})?: (.*)", w)):
                 ok = 1
                 if re.match("[A-Z]{1,15}: (.*)", w):
@@ -175,7 +193,19 @@ class GameOfThrones(SeriesPlugin):
                 ligne0 = uligne0.encode("ascii", "ignore")
                 uligne1 = ligne[1].decode("utf-8")
                 ligne1 = uligne1.encode("ascii", "ignore")
-                g.add_edge(t1, t2, spk=ligne0, speech=ligne1)
 
-        return g
+                # add /empty/ edge between previous and next annotations
+                t1 = T(episode=episode)
+                G.add_edge(t2, t1)
 
+                # add next annotation
+                t1 = t2
+                t2 = T(episode=episode)
+                G.add_edge(t1, t2, spk=ligne0, speech=ligne1)
+
+        # add /empty/ edge between previous annotation and episode end
+        t1 = t2
+        t2 = TEnd(episode=episode)
+        G.add_edge(t1, t2)
+
+        return G
